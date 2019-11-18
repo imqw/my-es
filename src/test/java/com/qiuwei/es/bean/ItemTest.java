1464b6412b90801a68bfca2b1f8f896db8d57081
qiuwei <Wcbc0310@126.com>
2019-11-18 17:26:12 +0800

    添加商品添加删除 单元测试

--- src/test/java/com/qiuwei/es/bean/ItemTest.java
+++ src/test/java/com/qiuwei/es/bean/ItemTest.java
@@ -1,6 +1,7 @@
 package com.qiuwei.es.bean;
 
 
+import com.qiuwei.es.mapper.ItemRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class ItemTest {
     @Autowired
     ElasticsearchTemplate template;
 
+    @Autowired
+    ItemRepository itemRepository;
+
+
     /**
      * 创建索引
      */
@@ -34,17 +39,48 @@ public class ItemTest {
 
     }
 
-
     /**
      * 删除索引
      */
 
     @Test
     public void delIndex() {
-
         boolean b = template.deleteIndex(Item.class);
-
         System.out.println(b);
+
+
+    }
+
+
+    /**
+     * 添加记录
+     */
+
+    @Test
+    public void addRecord() {
+        Item item = new Item();
+
+        item.setId(2L);
+        item.setTitle("小米手机7");
+        item.setBrand("小米");
+        item.setCategory("手机");
+        item.setPrice(3499.00);
+        item.setImages("http://image.baidu.com/13123.jpg");
+
+
+        boolean b = itemRepository.existsById(2L);
+
+        System.out.println("是否存在 " + b);
+
+        if (b) {
+            itemRepository.delete(item);
+            System.out.println("已经存在先删除");
+        }
+
+
+        itemRepository.save(item);
+        System.out.println("保存成功");
+
 
     }
 }
