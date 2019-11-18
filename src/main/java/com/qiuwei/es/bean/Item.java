86323b069913398a6f83d3d818e8c883200a0d64
qiuwei <Wcbc0310@126.com>
2019-11-18 16:10:41 +0800

    商品实例添加es属性配置

--- src/main/java/com/qiuwei/es/bean/Item.java
+++ src/main/java/com/qiuwei/es/bean/Item.java
@@ -1,35 +1,46 @@
 package com.qiuwei.es.bean;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * @Author: qiuwei@19pay.com.cn
  * @Date: 2019-11-18.
  */
 @Data
+@Document(indexName = "item",type = "docs")
 public class Item {
 
+    @Id
     private Long id;
     /**
      * 标题
      */
 
+    @Field(type = FieldType.Text)
     private String title;
     /**
      * 分类
      */
+    @Field(type = FieldType.Keyword)
     private String category;
     /**
      * 品牌
      */
+    @Field(type = FieldType.Keyword)
     private String brand;
     /**
      * 价格
      */
+    @Field(type = FieldType.Double)
     private Double price;
     /**
      * 图片地址
      */
+    @Field(index = false,type = FieldType.Keyword)
     private String images;
 
 }
